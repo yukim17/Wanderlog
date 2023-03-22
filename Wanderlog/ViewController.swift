5675c12d02774df959ccdca47c862b80437459de
egrishina <egrishina22@fed.idserve.net>
2023-03-22 10:40:31 +0100

    Initial Commit

---
+++ Wanderlog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wanderlog
+//
+//  Created by Ekaterina Grishina on 22/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
